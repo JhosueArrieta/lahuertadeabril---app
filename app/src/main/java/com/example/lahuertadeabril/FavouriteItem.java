41a22e53f0e95c47c82f42978aa2d41741cfebf2
Jhosue <jarrietac22@fpcoruna.afundacion.org>
2024-06-05 11:00:55 +0200

    DetailsScreen completadas y comprobadas, falta revisar un detalle del teclado en SearchScreen

--- app/src/main/java/com/example/lahuertadeabril/FavouriteItem.java
+++ app/src/main/java/com/example/lahuertadeabril/FavouriteItem.java
@@ -8,13 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class FavouriteItem1 extends AppCompatActivity {
+public class FavouriteItem extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_favourite_item1);
+        setContentView(R.layout.activity_favourite_item);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
